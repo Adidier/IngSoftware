5a73b1cac4ba59a6c8d40d21cbad725838caeb54
SLEEKHELL\juana <juareztamayojuan@gmail.com>
2023-10-09 08:47:42 -0600

    "generacion vector class y funcion add"

--- include/Vector.h
+++ include/Vector.h
@@ -1,3 +1,12 @@
 #include <iostream>
 
-void suma();
+class Vector3{
+    public:
+    float m_x,m_y,m_z; //Variables para el vector 3D sisi
+    public:
+    Vector3(float x,float y, float z); //Constructor genial
+    float getX();
+    float getY();
+    float getZ();
+    Vector3 add(const Vector3 &addVector); // Vector que suma al vector original otro vector
+};
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include"Vector.h"
 
-void suma()
+/**
+ * @brief Suma otro vector a la instancia actual.
+ *
+ * Esta función toma un objeto Vector3 por referencia, aunque si eres malo en age como Kevin no le vas a entender >:c, lo suma a la instancia
+ * actual y devuelve un nuevo objeto Vector3 que contiene el resultado de la suma.
+ *
+ * @param addVector Un objeto Vector3 que se sumará al objeto actual.
+ *
+ * @return Un nuevo objeto Vector3 que representa la suma de la instancia actual
+ *         y addVector.
+ * @author [Alfredo :c no sirve la docuemntacion tache :c]
+ */
+
+
+Vector3::Vector3(float t_x, float t_y, float t_z)
 {
-    
+    m_x = t_x;
+    m_y = t_y;
+    m_z = t_z; 
+}
+
+Vector3 Vector3::add(const Vector3 &addVector)
+{
+    //addVector.m_x = 10f;
+    this->m_x += addVector.m_x;
+    this->m_y += addVector.m_y;
+    this->m_z += addVector.m_z;
+    return Vector3(this->m_x,this->m_y,this->m_z);
 }
