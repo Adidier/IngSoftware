081749e0d2c589aa761bc68c043a3de5c43b8da7
Adidier <adidier.perez@gmail.com>
2023-10-18 06:36:31 -0600

    hack

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-    Vector3 v1(2, 3, 4);
+   /* Vector3 v1(2, 3, 4);
     Vector3 v2(6, 3, 7);
     float dot;
 
     dot = Vector3::dot(v1, v2);
     std::cout << "The result of the dot product between the v1 and v2 is: " << dot
          << "\n";
+
+         */
 }
