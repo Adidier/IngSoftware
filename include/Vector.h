ca74e5b86a6654798167c72090fca807d4578e96
RodrigoGL1251 <92412932+RodrigoGL1251@users.noreply.github.com>
2023-11-27 08:47:45 -0600

    Documentacion de funciones

--- include/Vector.h
+++ include/Vector.h
@@ -10,14 +10,80 @@ class Vector3{
     void direction();
     public:
 
-    Vector3(float t_x, float t_y, float t_z); //Constructor genial
-    float getX(); //get valor x
-    float getY();//get valor y
-    float getZ();//get valor z
+        /**
+     * @brief Constructor con parametros
+     *
+     * @param x Componente x del vector
+     * * @param y Componente y del vector
+     * * @param z Componente z del vector
+     * @return
+     */
+
+    Vector3(float t_x, float t_y, float t_z); 
+    /**
+     * @brief Regresa la componente "x" del vector
+     *
+     * @param
+     * @return float El valor de la componente "x"
+     */
+
+    float getX(); 
+    /**
+     * @brief Regresa la componente "y" del vector
+     *
+     * @param
+     * @return float El valor de la componente "y"
+     */
+
+    float getY();
+    /**
+     * @brief Regresa la componente "z" del vector
+     *
+     * @param
+     * @return float El valor de la componente "z"
+     */
+
+    float getZ();
+    /**
+     * @brief Suma de dos vectores
+     *
+     * @param addVector El vector que se va a sumar
+     * @return Vector3 El resultado de la suma del vector original con el que se le pasa de parametro
+     */
+
     Vector3 add(const Vector3 &addVector); // Vector que suma al vector original otro vector
+    /**
+     * @brief Multiplicacion del vector por un escalar
+     *
+     * @param t_scalar El escalar por el que se va a multiplicar
+     * @return Vector3 El resultado de multiplicar el vector original por el escalar que se le paso como parametro
+     */
+
     Vector3 multiScalar(float &t_scalar); //Vector que se multiplica por un escalar
+    /**
+     * @brief Producto cruz entre dos vectores
+     *
+     * @param t_v1 Vector uno para el producto cruz
+     * * @param t_v2 Vector uno para el producto cruz
+     * @return Vector3 El resultado del producto cruz de los vectores
+     */
+
     static Vector3 cross(Vector3 &t_v1, Vector3 &t_v2);
+    /**
+     * @brief Producto punto entre dos vectores
+     *
+     * @param t_v1 Vector uno para el producto punto
+     * * @param t_v2 Vector uno para el producto punto
+     * @return float El resultado del producto punto de los vectores
+     */
+
     static float dot(const Vector3 &t_v1, const Vector3 &t_v2);
+    /**
+     * @brief Resta de dos vectores
+     *
+     * @param addVector El vector que se va a restar
+     * @return Vector3 El resultado de la resta del vector original con el que se le pasa de parametro
+     */
     Vector3 sub(const Vector3& subVector);
     float getDirectionAxisX(); // float que regresa la direccion de x
     float getDirectionAxisY(); // float que regresa la direccion de y
