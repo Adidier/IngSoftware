16a1089c3273cfe353153296b7891f5ea1058233
RodrigoGL1251 <92412932+RodrigoGL1251@users.noreply.github.com>
2023-12-13 08:37:40 -0600

    Draw map (#11)
    * Carga de sprites mapa
    
    * map draw
    
    * Dibujando textura en orden, modificacion de textura, configuracion de mapa enformato txt
    
    * Archivo txt
    
    * Modificacion de w y h

--- game/include/ResourceManager.h
+++ game/include/ResourceManager.h
@@ -9,6 +9,7 @@ public:
 	static ResourceManager* GetPtr();
 	void Load(std::string file);
 	Object* GetResource(std::string resource);
+	static std::string readTxtFile(std::string filePath);
 private:
 	static ResourceManager* ptr;
 	std::map<std::string, Object *> resources;
